aaaff7d5a94e53a9f07e3243dac18a3fd97adc7b
Andrew M Williams <drewmikewill@engineer.com>
2018-12-11 22:22:11 -0500

    First draft of interleave

---
+++ TechnicalScreening/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace TechnicalScreening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstTestString = ProgrammingQuestions.Interleave("abc", "12345");
+            string secondTestString = ProgrammingQuestions.Interleave("abcdef", "123");
+            string thirdTestString = ProgrammingQuestions.Interleave("abc", "123");
+            string fourthTestString = ProgrammingQuestions.Interleave(null, "123");
+            string fifthTestString = ProgrammingQuestions.Interleave("abc", null);
+            string sixthTestString = ProgrammingQuestions.Interleave(null , null);
+            Console.WriteLine(firstTestString);
+            Console.WriteLine(secondTestString);
+            Console.WriteLine(thirdTestString);
+            Console.WriteLine(fourthTestString);
+            Console.WriteLine(fifthTestString);
+            Console.WriteLine(sixthTestString);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TechnicalScreening/ProgrammingQuestions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechnicalScreening
+{
+    internal static class ProgrammingQuestions
+    {
+        /// <summary>
+        /// Takes two strings and does the "perfect shuffle" with those two strings, alternating characters from the two input strings. 
+        /// </summary>
+        /// <param name="firstString">First character of this string will assume the first position in the string returned</param>
+        /// <param name="secondString">First character of this string will take the second position in the string returned</param>
+        /// <returns>String that is the product of the two parameters interleaved</returns>
+        internal static string Interleave(string firstString, string secondString)
+        {
+            if (firstString == null && secondString == null)
+                return null;
+            if (firstString == null)
+                return secondString;
+            if (secondString == null)
+                return firstString;
+
+            int firstStringLength = firstString.Length, secondStringLength = secondString.Length;
+            int count = 0;
+            string finalString = null;
+            List<char> finalStringChars = new List<char>();
+
+            if (firstStringLength <= secondStringLength)
+            {
+                while (count < firstStringLength)
+                {
+                    finalStringChars.Add(firstString[count]);
+                    finalStringChars.Add(secondString[count]);
+                    ++count;
+                }
+                finalString = string.Concat(string.Join(null, finalStringChars.ToArray()), secondString.Substring(count));
+            }
+            else
+            {
+                while (count < secondStringLength)
+                {
+                    finalStringChars.Add(firstString[count]);
+                    finalStringChars.Add(secondString[count]);
+                    ++count;
+                }
+                finalString = string.Concat(string.Join(null, finalStringChars.ToArray()), firstString.Substring(count));
+            }
+            return finalString;
+        }
+    }
+}
